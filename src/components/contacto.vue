f37a90042ffb713156a55537f615f2185788adda
Juan David <jdavid.sanchez@udea.edu.co>
2017-10-21 12:35:18 -0500

    First commit

---
+++ src/components/contacto.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Contacto</h1>
+  <ul>
+    <li v-for="item in arreglo">
+      {{item}}
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+  return {
+    arreglo:[
+      'Comenzar el ejemplo',
+      'Terminar el ejemplo'
+    ]
+  }
+
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <input type="text" placeholder="Texto" v-model="texto">
+    <input type="button" value="Cambiar Texto" v-on:click="cambiarTexto">
+    <p>{{texto2}}</p>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      texto: null,
+      texto2: null
+    }
+  },
+  methods:{
+    cambiarTexto(){
+      this.texto2 = this.texto;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
